8f8abb67f62616242ef0d7a188eddd8558553fe6
DanielLasota <01166109@pw.edu.pl>
2023-03-17 17:52:51 +0100

    update

--- Square F/main.cpp
+++ Square F/main.cpp
@@ -32,6 +32,8 @@ int main()
     double q = a * p * p + b * p + c;
     double r = a * (p + 1.0 / (4.0 * a)) * (p + 1.0 / (4.0 * a)) + q - 1.0 / (4.0 * a);
 
+    cout << "a=" << a << endl << "b=" << b << endl << "c=" << c << endl;
+
     // axes
     VertexArray axes(Lines, 4);
     axes[0].position = Vector2f(static_cast<float>(width / 2), 0.0f);
@@ -44,8 +46,7 @@ int main()
     axes[3].color = Color::Black;
 
     //############################################
-
-    // grid
+    //################# GRID #####################
     VertexArray grid(Lines, (width + height) * 2);
     int index = 0;
     for (int i = -width / 2; i <= width / 2; i++) {
@@ -57,7 +58,6 @@ int main()
             index += 2;
         }
     }
-
     for (int i = -height / 2; i <= height / 2; i++) {
         if (i != 0) {
             grid[index].position = Vector2f(0.0f, static_cast<float>(height / 2 + i));
@@ -67,7 +67,6 @@ int main()
             index += 2;
         }
     }
-
     // additional grid for 1x1 spacing
     VertexArray grid1x1(Lines, (width + height) * 2);
     index = 0;
@@ -80,7 +79,6 @@ int main()
             index += 2;
         }
     }
-
     for (int i = -height / 2; i <= height / 2; i++) {
         if (i % 10 == 0) {
             grid1x1[index].position = Vector2f(static_cast<float>(width / 2) - 0.5f, static_cast<float>(height / 2 + i));
@@ -90,17 +88,17 @@ int main()
             index += 2;
         }
     }
-
+    //############################################
     //############################################
 
     // function graph
     VertexArray plot(LineStrip, n);
     for (int i = 0; i < n; i++)
     {
-        //x = static_cast<float>(i - n / 2) / n * 20.0f;
         x = static_cast<float>(i - n / 2) / static_cast<float>(n) * 20.0f;
         y = a * x * x + b * x + c;
-        plot[i].position = Vector2f((x - p) * static_cast<float>(width) / 20.0f + static_cast<float>(width / 2), -(y - q) * static_cast<float>(height) / 20.0f + static_cast<float>(height / 2));
+        plot[i].position = Vector2f(x * static_cast<float>(width) / 20.0f + static_cast<float>(width / 2), -y * static_cast<float>(height) / 20.0f + static_cast<float>(height / 2));
+        //plot[i].position = Vector2f((x - p) * static_cast<float>(width) / 20.0f + static_cast<float>(width / 2), -(y - q) * static_cast<float>(height) / 20.0f + static_cast<float>(height / 2));
         plot[i].color = Color::Red;
     }
 
@@ -119,7 +117,7 @@ int main()
 
     sf::Text zeros; // ustawienie czcionki dla tekstu
     zeros.setFont(font);
-    zeros.setCharacterSize(20); // ustawienie rozmiaru tekstu domyœlnej czcionki
+    zeros.setCharacterSize(25); // ustawienie rozmiaru tekstu domyœlnej czcionki
     zeros.setFillColor(sf::Color::Black); // ustawienie koloru tekstu
 
     // ustawienie treœci tekstu na podstawie miejsc zerowych funkcji
@@ -176,7 +174,7 @@ int main()
 
 
         window.clear(Color::White); // czyszczenie ekranu
-        window.draw(grid);
+        //window.draw(grid);
         window.draw(grid1x1);
         window.draw(axes);  // rysowanie osi uk³adu wspó³rzêdnych
         window.draw(plot);  // rysowanie wykresu funkcji kwadratowe   
